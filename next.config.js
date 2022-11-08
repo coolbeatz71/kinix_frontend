ad58fa778a840abd6f0651de7ff22b1ffc43762f
coolbeatz71 <sigmacool@gmail.com>
2022-11-08 05:25:06 +0200

    chore: remove next-compose-plugins to fix next-config warnings

--- next.config.js
+++ next.config.js
@@ -1,35 +1,18 @@
 const path = require('path');
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-const withPlugins = require('next-compose-plugins');
 const withLess = require('next-with-less');
+const runtimeCaching = require('next-pwa/cache');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const pwaConfig = {
-    pwa: {
-        dest: 'public',
-        register: true,
-        runtimeCaching,
-        disable: !isProduction,
-    },
-};
-
 const antdLessVariables = path.resolve('./theme/variables.less');
 
-const antdLessConfig = {
-    lessLoaderOptions: {
-        additionalData: (content) => `${content}\n\n@import '${antdLessVariables}';`,
-    },
-};
-
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = withPlugins([[withPWA, pwaConfig], [withLess, antdLessConfig], [withBundleAnalyzer]], {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    swcMinify: true,
     webpack(config) {
         config.resolve.alias['@ant-design/icons/lib/dist$'] = path.join(__dirname, './icons.js');
         return config;
@@ -43,4 +26,18 @@ module.exports = withPlugins([[withPWA, pwaConfig], [withLess, antdLessConfig], 
             'platform-lookaside.fbsbx.com',
         ],
     },
-});
+    reactStrictMode: true,
+    lessLoaderOptions: {
+        additionalData: (content) => `${content}\n\n@import '${antdLessVariables}';`,
+    },
+    pwa: {
+        dest: 'public',
+        register: true,
+        runtimeCaching,
+        disable: !isProduction,
+    },
+};
+
+const plugins = [withPWA, withLess, withBundleAnalyzer];
+
+module.exports = () => plugins.reduce((config, plugin) => plugin(config), nextConfig);
